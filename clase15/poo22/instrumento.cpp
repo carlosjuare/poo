3f4dd2a6bd99e4fdffc334bd91fac2f2778aaf57
carlosjuare <119368143+carlosjuare@users.noreply.github.com>
2024-06-21 20:42:30 -0300

    Delete clase15 directory

--- clase15/poo22/instrumento.cpp
+++
@@ -1,9 +0,0 @@
-#include "instrumento.h"
-
-Instrumento::Instrumento() : marca("Yamaha") {}
-
-Instrumento::~Instrumento() {}
-
-void Instrumento::verlo() const {
-    std::cout << "Marca: " << marca << std::endl;
-}
